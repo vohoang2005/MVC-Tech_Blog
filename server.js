37ac5289e579c817091f563ed6ca34b4df23d909
vohoang2005 <vohoang2005@gmail.com>
2022-02-13 10:55:10 -0800

    updated

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.use(session(sess));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(process.env.PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening'));
 });
