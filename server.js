e8feb7768598f227eaed3c701c245ccab97ce84c
vohoang2005 <vohoang2005@gmail.com>
2022-02-13 10:47:53 -0800

    updated port

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const sess = {
   };
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
